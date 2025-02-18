9a0bd4656f35401d6b3054a9b16b78561949d8e3
Quang <nguyenvanquang247@gmail.com>
2025-02-18 16:06:25 +0700

    Add membership page with dynamic membership parsing and images

---
+++ content/join-as-a-member.md
@@ -0,0 +1,21 @@
+title: Join as a Member
+slug: join-as-a-member
+Date: 2025-02-18    
+template: join-as-a-member
+memberships:
+    - name: Senior Member
+      description: Annual Senior Membership. Play as much as you like, join in our events and sign up for group lessons! 
+      price: 100
+      image: SeniorMembership.jpg
+    - name: Family Membership
+      description: Why not play as a family? Our family membership provides great enjoyment for the entire family! <br/>Please note over 18's living at home are considered student members if they are in 3rd level education. 
+      price: 165
+      image: FamilyMembership.jpg
+    - name: Junior Member (U/18)
+      description: We have just opened up our membership to junior players and look forward to offering lots of fun events for these upcoming stars! 
+      price: 60
+      image: JuniorMembership.jpg
+    - name: Young Adult Membership
+      description: Any person who is aged 18 to 23 years of ages.
+      price: 60
+      image: Student.jpg
--- pelicanconf.py
+++ pelicanconf.py
@@ -42,6 +42,31 @@ EXTRA_PATH_METADATA = {
     'static/favicon.ico': {'path': 'favicon.ico'}
 }
 
+def parse_memberships(membership_list):
+    memberships = []
+    current_membership = {}
+
+    for item in membership_list:
+        if item.startswith('- name:'):
+            if current_membership:
+                memberships.append(current_membership)
+            current_membership = {'name': item.split(': ', 1)[1]}
+        elif item.startswith('description:'):
+            current_membership['description'] = item.split(': ', 1)[1]
+        elif item.startswith('price:'):
+            current_membership['price'] = item.split(': ', 1)[1]
+        elif item.startswith('image:'):
+            current_membership['image'] = item.split(': ', 1)[1]
+
+    if current_membership:
+        memberships.append(current_membership)
+
+    return memberships
+
+JINJA_FILTERS = {
+    'parse_memberships': parse_memberships,
+}
+
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
